fef5d3f77fa7cdba924823931e9f40c1dd640214
PS Subramanya Bhat <subramanya@robosoftin.com>
2020-11-11 03:13:43 +0530

    Added view info feature for birthday reminder app;ication

--- src/List.js
+++ src/List.js
@@ -1,30 +1,34 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import cn from 'classnames';
 const List = ({ people }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
-    // <>
-    //   {people.map((person) => {
-    //     const { id, name, age, image } = person;
-    //     return <article key={id} className="person">
-    //       <img src={image} alt={name} />
-    //       <div>
-    //         <h4>{name}</h4>
-    //         <p>{age} years</p>
-    //       </div>
-    //     </article>
-    //   })}
-    // </>
-    
     <div class="underline">
       {people.map((person) => {
-        const { id, name, age, image } = person;
+        const { id, name, age, image, workplace, description } = person;
         return <article key={id} className="person">
-          <img src={image} alt={name} />
+          <img class={cn('normalview', { detailedview: showDetails})} src={image} alt={name} />
           <div>
-            <h4>{name}</h4>
+            <h4 class="birthdayinfo">{name}</h4>
             <p>{age} years</p>
+            {/* <p>{workplace} years</p>
+            <p>{description} years</p> */}
+            <p class="companyinfo">{showDetails ? workplace : `${workplace.substring(0, 0)}`}</p>
+            <p class="designation">{showDetails ? description : `${description.substring(0, 0)}`}</p>
+            <p>
+              <button class="infobtn"
+                onClick={() => {
+                  setShowDetails(!showDetails);
+                }}
+              >
+                {showDetails ? 'Close' : 'Show Details'}
+              </button>
+            </p>
           </div>
+
         </article>
+
       })}
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -5,6 +5,8 @@ export default [
     age: 29,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    workplace: 'Yahoo Company',
+    description: 'Hi, I am a front end developer, and I like to draw and sing',
   },
   {
     id: 2,
@@ -12,6 +14,8 @@ export default [
     age: 32,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    workplace: 'Cognizant Pvt. Ltd.',
+    description: 'Hi, I am a testing developer and I take care of testing a lot of applications that we provide to our customers',
   },
   {
     id: 3,
@@ -19,6 +23,8 @@ export default [
     age: 36,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    workplace: 'Mind Tree Pvt. Ltd.',
+    description: 'Hi, I am a data scientist and I take care about the data analytical part of most of the projects',
   },
   {
     id: 4,
@@ -26,12 +32,16 @@ export default [
     age: 34,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    workplace: 'Infosys Pvt. Limited',
+    description: 'Hi, I am a back end developer, and I am currently working on developing a strong web application with a strong backend support',
   },
   {
     id: 5,
-    name: 'Lola Gardner',
-    age: 29,
+    name: 'PS Subramanya Bhat',
+    age: 22,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      'https://avatars0.githubusercontent.com/u/44722508?s=400&u=1de957431f890d616b8831cdd95329bbaae324d4&v=4',
+    workplace: 'Robosoft Pvt. Limited',
+    description: 'Hi, I am a Trainee Software Engineer who is currently working as a front end developer for mobile application using react Native',
   },
 ];
