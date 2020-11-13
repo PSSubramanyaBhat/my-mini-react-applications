9cbbe885c3c09e8358ea835164c4248b5fec6766
PS Subramanya Bhat <subramanya@robosoftin.com>
2020-11-14 03:15:54 +0530

    Added Birthday add festure in Semi perfect way

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import data from './data';
 import List from './List';
 import cn from 'classnames';
+
+import { readFromStorage, writeToStorage } from './LocalStorage';
 import {
   BsArrowLeftShort, BsArrowRightShort,
   BsFillGiftFill, BsAlarm,
@@ -16,15 +18,24 @@ const sampleList = [
   28
 ];
 
+const sampleid = [
+  6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
+];
+
+// const sampleData = [
+
+// ];
+
 const ENTRY = 'Entry';
 
-function App() {
-  // const val = new Set(data);
-  // const allEntries = [...val];
-  // const [people, setPeople] = useState(allEntries);
+const newvarnum = 5;
 
-  const [people, setPeople] = useState(data);
+function App() {
+  // const [people, setPeople] = useState(data);
+  const [people, setPeople] = useState(() => readFromStorage(ENTRY) || []);
   const [refresh, setRefresh] = useState(false);
+
+  const [generateID, setGenerateID] = useState(6);
 
   const [dateSelected, setDateSelected] = useState(0);
 
@@ -35,17 +46,12 @@ function App() {
     // return dispMsg[messageNumber];
   };
   */
-  // const [people, setPeople] = useState(() => readFromStorage(ENTRY) || []);
 
+  useEffect(() => {
+    writeToStorage(ENTRY, data);
+  });
 
-
-  // useEffect(() => {
-  //   console.log(data);
-  //   setPeople(data);
-  // },);
-
-
-  const newObj = {}
+  // const newObj = {}
   return <main>
     <section className="container">
 
@@ -92,7 +98,7 @@ function App() {
         }
         <button class="addbutton"
           onClick={() => {
-            data.push({
+            /*data.push({
               id: 1,
               name: 'Bertie Yates',
               age: 29,
@@ -100,10 +106,29 @@ function App() {
                 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
               workplace: 'Yahoo Company',
               description: 'Hi, I am a front end developer, and I like to draw and sing',
-            });
+            });*/ //PARTIALLY WORKING......
+
+
+            setGenerateID((x) => x + 1);
+            // newvarnum += 1;
+            const newObj = {
+              // id: 1,
+              id: generateID,
+              name: 'Bertie Yates',
+              age: 29,
+              image:
+                'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+              workplace: 'Yahoo Company',
+              description: 'Hi, I am a front end developer, and I like to draw and sing',
+            };
+
+            data.push(newObj);
 
             // console.log(data);
             // setPeople(data);
+
+            // setPeople([...data, newObj]);
+            writeToStorage(ENTRY, data);
 
           }}
         >
